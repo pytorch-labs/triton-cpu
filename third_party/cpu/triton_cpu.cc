6ab867b14528fb878d166490c232368693b0d2e9
Minjang Kim <minjang@meta.com>
2024-05-01 16:42:14 -0700

    Run clang-format

--- third_party/cpu/triton_cpu.cc
+++ third_party/cpu/triton_cpu.cc
@@ -1,24 +1,24 @@
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/Support/TargetSelect.h"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
-#include "llvm/IR/Constants.h"
-#include "llvm/Support/TargetSelect.h"
-#include "mlir/Pass/Pass.h"
-#include "mlir/Pass/PassManager.h"
 
 #include <iostream>
 
 namespace py = pybind11;
 
-void init_triton_passes_ttcpuir(py::module&& m) {
+void init_triton_passes_ttcpuir(py::module &&m) {
   // TODO:
 }
 
-void init_triton_cpu(py::module&& m) {
+void init_triton_cpu(py::module &&m) {
   auto passes = m.def_submodule("passes");
   init_triton_passes_ttcpuir(passes.def_submodule("ttcpuir"));
 
-  m.def("load_dialects", [](mlir::MLIRContext& context) {
+  m.def("load_dialects", [](mlir::MLIRContext &context) {
     // TODO:
   });
 }
